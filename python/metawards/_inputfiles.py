082c060ab705a06ce841484996aef8c653f0c954
chryswoods <chryswoods@gmail.com>
2020-03-26 14:53:16 +0000

    Input files are found correctly. Switching to more pythonic variable names. Factoring out variables from the Parameters class

--- python/metawards/_inputfiles.py
+++ python/metawards/_inputfiles.py
@@ -14,29 +14,30 @@ _2011Data = os.path.join(os.path.dirname(__file__),
 
 @dataclass
 class InputFiles:
-    WorkName: str = None            # WORK MATRIX
-    PlayName: str = None            # PLAY MATRIX
-    IdentifierName: str = None      # WARD NAMES
-    IdentifierName2: str = None     # WARD IDs (Communities, Counties,
+    work: str = None                # WORK MATRIX
+    play: str = None                # PLAY MATRIX
+    identifier: str = None          # WARD NAMES
+    identifier2: str = None         # WARD IDs (Communities, Counties,
                                     #           Districts, UA's etc)
-    WeekendName: str = None         # WEEKEND MATRIX
-    PlaySizeName: str = None        # SIZE OF POPULATION IN THE PLAY PILE
-    PositionName: str = None        # CENTRE of BOUNDING BOXES
-    SeedName: str = None            # LIST OF SEED NODES
-    NodesToTrack: str = None        # LIST OF NODES TO TRACK
-    AdditionalSeeding: str = None   #LIST OF EXTRA SEED WARDS...
+    weekend: str = None             # WEEKEND MATRIX
+    play_size: str = None           # SIZE OF POPULATION IN THE PLAY PILE
+    position: str = None            # CENTRE of BOUNDING BOXES
+    seed: str = None                # LIST OF SEED NODES
+    nodes_to_track: str = None      # LIST OF NODES TO TRACK
+    additional_seeding: str = None  # LIST OF EXTRA SEED WARDS...
+    uv: str = None                  # UV file
 
     def __str__(self):
-        return f"WorkName = {self.WorkName}\n" \
-               f"PlayName = {self.PlayName}\n" \
-               f"IdentifierName = {self.IdentifierName}\n" \
-               f"IdentifierName2 = {self.IdentifierName2}\n" \
-               f"WeekendName = {self.WeekendName}\n" \
-               f"PlaySizeName = {self.PlaySizeName}\n" \
-               f"PositionName = {self.PositionName}\n" \
-               f"SeedName = {self.SeedName}\n" \
-               f"NodesToTrack = {self.NodesToTrack}\n" \
-               f"AdditionalSeeding = {self.AdditionalSeeding}\n"
+        return f"work = {self.work}\n" \
+               f"play = {self.play}\n" \
+               f"identifier = {self.identifier}\n" \
+               f"identifier2 = {self.identifier2}\n" \
+               f"weekend = {self.weekend}\n" \
+               f"play_size = {self.play_size}\n" \
+               f"position = {self.position}\n" \
+               f"seed = {self.seed}\n" \
+               f"nodes_to_track = {self.nodes_to_track}\n" \
+               f"additional_seeding = {self.additional_seeding}\n"
 
     @staticmethod
     def set_files(name: str, files):
@@ -64,14 +65,16 @@ class InputFiles:
 # "set_files" function above to change this though
 _key4 = InputFiles()
 
-_key4.WorkName = os.path.join(_2011Data, "EW1.dat")
-_key4.PlayName = os.path.join(_2011Data, "PlayMatrix.dat")
-_key4.PlaySizeName = os.path.join(_2011Data, "PlaySize.dat")
-_key4.PositionName = os.path.join(_2011Data, "CBB2011.dat")
-_key4.SeedName = os.path.join(_2011Data, "seeds.dat")
-_key4.NodesToTrack = os.path.join(_2011Data, "seeds.dat")
-_key4.AdditionalSeeding = os.path.join(_2011Data, "ExtraSeedsBrighton.dat")
-_key4.UVFilename = os.path.join(_2011Data, "UVScaling.csv")
-_key4.WeekendName = None   # this is not set in the original code?
+_key4.work = os.path.join(_2011Data, "EW1.dat")
+_key4.play = os.path.join(_2011Data, "PlayMatrix.dat")
+_key4.play_size = os.path.join(_2011Data, "PlaySize.dat")
+_key4.position = os.path.join(_2011Data, "CBB2011.dat")
+_key4.seed = os.path.join(_2011Data, "seeds.dat")
+_key4.nodes_to_track = os.path.join(_2011Data, "seeds.dat")
+_key4.additional_seeding = os.path.join(_2011Data, "ExtraSeedsBrighton.dat")
+_key4.uv = os.path.join(_2011Data, "UVScaling.csv")
+_key4.weekend = None     # this is not set in the original code?
+_key4.identifier = None  # this is not set in the original code?
+_key4.identifier2 = None # this is not set in the original code?
 
 InputFiles.set_files(4, _key4)
