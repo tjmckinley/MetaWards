fc7068279dd3d35fa95dccab30055237dc8218c4
GitHub Action <action@github.com>
2020-04-05 14:41:22 +0000

    Update webstite.

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g2790708',
+    VERSION: '0+untagged.1.g7744afa',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
