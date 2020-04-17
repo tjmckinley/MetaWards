3633ee840eb80ced62243b1d8eb2f09557ab9dfa
chryswoods <chryswoods@gmail.com>
2020-04-17 17:01:16 +0100

    Finally got the iterator importing from a string working. Very interesting python in build_custom_iterator, and lots of tests in test_custom_iterator

---
+++ tests/test_custom_iterator.py
@@ -0,0 +1,64 @@
+
+from metawards.iterators import build_custom_iterator, iterate_default, \
+                                iterate_weekday
+
+
+def my_iterator(**kwargs):
+    return iterate_weekday(**kwargs)
+
+
+def iterate_customised(**kwargs):
+    return iterate_weekday(**kwargs)
+
+
+def test_my_custom_iterator():
+    default_funcs = iterate_default()
+
+    # make sure that build_custom_iterator doesn't change iterate_default
+    custom = build_custom_iterator(iterate_default)
+
+    custom_funcs = custom()
+
+    assert custom_funcs == default_funcs
+
+    # customise from existing function
+    custom = build_custom_iterator(my_iterator)
+
+    custom_funcs = custom()
+
+    assert custom_funcs == default_funcs
+
+    # customise from existing function that is in the metawards.iterators
+    # namespace
+    custom = build_custom_iterator("iterate_weekday")
+
+    custom_funcs = custom()
+
+    assert custom_funcs == default_funcs
+
+    # customise from existing function that is already imported
+    custom = build_custom_iterator("my_iterator")
+
+    custom_funcs = custom()
+
+    assert custom_funcs == default_funcs
+
+    # customise from loading the first 'iterate_XXX' function from
+    # the specified module (in this case, this script)
+    custom = build_custom_iterator("test_custom_iterator")
+
+    custom_funcs = custom()
+
+    assert custom_funcs == default_funcs
+
+    # customise from loading the specified function in the
+    # specified module (in this case, this script)
+    custom = build_custom_iterator("test_custom_iterator::my_iterator")
+
+    custom_funcs = custom()
+
+    assert custom_funcs == default_funcs
+
+
+if __name__ == "__main__":
+    test_my_custom_iterator()
