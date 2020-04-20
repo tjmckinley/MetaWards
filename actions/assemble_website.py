35f5b1e4a4c7240817057d51731501df9808066e
chryswoods <chryswoods@gmail.com>
2020-04-20 10:35:13 +0100

    Working - some dummy commits coming...

--- actions/assemble_website.py
+++ actions/assemble_website.py
@@ -3,6 +3,7 @@ import sys
 import os
 from distutils import dir_util
 import glob
+import json
 
 branch = metawards.__branch__
 release = metawards.__version__
@@ -17,7 +18,7 @@ print(f"Build docs for branch {branch} version {version}")
 # we will only build docs for the master and devel branches
 # (as these are moved into special locations)
 
-if branch not in ["master", "devel", "feature_tutorial"]:
+if branch not in ["master", "devel"]:
     print(f"We don't assemble the website for branch {branch}")
     sys.exit(0)
 
@@ -34,17 +35,34 @@ if not os.path.exists("./gh-pages"):
 # if this is the master branch, then copy the docs to both the root
 # directory of the website, and also to the 'versions/version' directory
 if branch == "master":
+    print(f"Copying main docs to gh-pages and gh-pages/versions/{version}")
     dir_util.copy_tree("doc/build/html/", "gh-pages/")
-    dir_util.copy_tree("doc/build/html/", f"gh-pages/versions/${version}/")
+    dir_util.copy_tree("doc/build/html/", f"gh-pages/versions/{version}/")
 
 elif branch == "devel":
     dir_util.copy_tree("doc/build/html/", "gh-pages/versions/devel/")
 
 # now write the versions.json file
-versions = {}
-versions["latest"] = "/"
-versions["development"] = "/versions/devel/"
+versions = []
+versions.append(["latest", "/"])
+versions.append(["development", "/versions/devel/"])
+
+vs = {}
 
 for version in glob.glob("gh-pages/versions/*"):
-    print(version)
+    if version.find("devel") == -1:
+        version = version.split("/")[-1]
+        vs[version] = f"/versions/{version}/"
 
+# remove / deduplicate files into symlinks
+
+keys = list(vs.keys())
+keys.sort()
+
+for i in range(len(keys)-1, -1, -1):
+    versions.append([keys[i], vs[keys[i]]])
+
+print(f"Saving paths to versions\n{versions}")
+
+with open("gh-pages/versions.json", "w") as FILE:
+    json.dump(versions, FILE)
