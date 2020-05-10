8f72d6412d22dc4f289c6b18782d9c26f42f2afc
GitHub Action <action@github.com>
2020-05-10 17:23:18 +0000

    Update website.

---
+++ _sources/api/index_api_MetaWards_mixers.rst.txt
@@ -0,0 +1,8 @@
+===========================
+MetaWards.mixers API Detail
+===========================
+
+.. automodule:: metawards.mixers
+
+.. toctree::
+   :maxdepth: 1
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.10.0',
+    VERSION: '0.10.0+114.g3028710',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
