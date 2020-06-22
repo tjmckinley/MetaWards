c6fd14f2fe06ecebf49844ff6688befc4e6087c5
chryswoods <chryswoods@gmail.com>
2020-06-22 18:16:11 +0100

    Data for custom disease stages is now output to both the results and trajectory files. This looks like it is now all working. I can progress with the tutorial for this feature :-)

--- src/metawards/extractors/_output_final_report.py
+++ src/metawards/extractors/_output_final_report.py
@@ -35,8 +35,19 @@ look at statistics across all runs using e.g. R or pandas""",
     else:
         datestring = ""
 
+    # get the first Population in the trajectory, as this will give
+    # us the list of extra disease stages to print out
+    t0 = results[0][-1][0]
+
+    if t0.totals is None:
+        extra_stages = []
+        extra_str = ""
+    else:
+        extra_stages = list(t0.totals.keys())
+        extra_str = ",".join(extra_stages) + ","
+
     RESULTS.write(f"fingerprint,repeat,{varnames}"
-                  f"day,{datestring}S,E,I,R,IW,UV\n")
+                  f"day,{datestring}S,E,I,{extra_str}R,IW,UV\n")
     for varset, trajectory in results:
         varvals = varset.variable_values()
         if varvals is None or len(varvals) == 0:
@@ -53,7 +64,20 @@ look at statistics across all runs using e.g. R or pandas""",
             else:
                 d = ""
 
+            if len(extra_stages) > 0:
+                extra_vals = []
+
+                for stage in extra_stages:
+                    if pop.totals is None:
+                        extra_vals.append("0")
+                    else:
+                        extra_vals.append(str(pop.totals.get(stage, 0)))
+
+                extra_str = ",".join(extra_vals) + ","
+            else:
+                extra_str = ""
+
             RESULTS.write(f"{start}{pop.day},{d}{pop.susceptibles},"
-                          f"{pop.latent},{pop.total},"
+                          f"{pop.latent},{pop.total},{extra_str}"
                           f"{pop.recovereds},{pop.n_inf_wards},"
                           f"{pop.scale_uv}\n")
