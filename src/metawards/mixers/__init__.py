eece44f3efe64a569287773960344c644d748627
Christopher Woods <Christopher.Woods@bristol.ac.uk>
2021-02-03 17:39:55 +0000

    Added a new mixer that takes the number of individuals in each ward properly into account when merging FOIs using an interaction matrix.

--- src/metawards/mixers/__init__.py
+++ src/metawards/mixers/__init__.py
@@ -10,6 +10,8 @@ Functions
     InteractionMatrix
 
     merge_evenly
+    merge_using_matrix
+    merge_matrix_density
 
     mix_custom
     mix_default
@@ -22,6 +24,7 @@ from ._interaction_matrix import *
 
 from ._merge_evenly import *
 from ._merge_using_matrix import *
+from ._merge_matrix_density import *
 
 from ._mix_custom import *
 from ._mix_default import *
